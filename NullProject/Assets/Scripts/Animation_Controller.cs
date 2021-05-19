ddfa29a3fa6e1c22650e0c127c9c3ac4b85cb441
Relicjamin-jv <campbellcl19@gcc.edu>
2021-05-18 21:23:59 -0400

    Finished with the animation for point and click
    Movement script for all characters

--- NullProject/Assets/Scripts/Animation_Controller.cs
+++ NullProject/Assets/Scripts/Animation_Controller.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Animation_Controller : MonoBehaviour
 {
     private Animator _anim; //animator var
+
+    private Vector3 _postion;
     // Start is called before the first frame update
     void Start()
     {
@@ -40,29 +42,30 @@ public class Animation_Controller : MonoBehaviour
 
         //seperation between point-to-click and wasd.
 
-        if(Input.GetKey(KeyCode.W)){ 
+        if((this.transform.position.y) > _postion.y && Mathf.Abs(this.transform.position.y - _postion.y) > Mathf.Abs(this.transform.position.x - _postion.x)){ 
             _anim.SetBool("Is_Walking_Up", true);
         }else{
             _anim.SetBool("Is_Walking_Up", false);
         }
 
-        if(Input.GetKey(KeyCode.S)){
+        if((this.transform.position.y) < _postion.y && Mathf.Abs(this.transform.position.y - _postion.y) > Mathf.Abs(this.transform.position.x - _postion.x)){
             _anim.SetBool("Is_Walking_Down", true);
         }else{
             _anim.SetBool("Is_Walking_Down", false);
         }
 
-        if(Input.GetKey(KeyCode.A)){
+        if(this.transform.position.x < _postion.x && Mathf.Abs(this.transform.position.x - _postion.x) > Mathf.Abs(this.transform.position.y - _postion.y)){
             _anim.SetBool("Is_Walking_Left", true);
         }else{
             _anim.SetBool("Is_Walking_Left", false);
         }
 
-        if(Input.GetKey(KeyCode.D)){
+        if(this.transform.position.x > _postion.x && Mathf.Abs(this.transform.position.x - _postion.x) > Mathf.Abs(this.transform.position.y - _postion.y)){
             _anim.SetBool("Is_Walking_Right", true);
         }else{
             _anim.SetBool("Is_Walking_Right", false);
         }
+        _postion = this.transform.position;
     }
 
     //seperation between point-to-click and wasd.
