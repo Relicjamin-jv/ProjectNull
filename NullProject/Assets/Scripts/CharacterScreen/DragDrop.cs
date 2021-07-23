2e03f8cc42525760ad7fe6dd677849a0e65ffd68
Relicjamin-jv <campbellcl19@gcc.edu>
2021-07-23 19:53:12 -0400

    Added pop up UI to Inv

--- NullProject/Assets/Scripts/CharacterScreen/DragDrop.cs
+++ NullProject/Assets/Scripts/CharacterScreen/DragDrop.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler,  IDragHandler
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler,  IDragHandler, IPointerEnterHandler, IPointerExitHandler
 {
     private RectTransform rectTransform;
     [SerializeField] private Canvas canvas;
     private CanvasGroup canvasGroup;
     private Vector2 orgin;
+    private ItemSlot itemInfo;
+    [SerializeField] GameObject objectGUI;
+   
 
     private void Awake() {
         rectTransform =  GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
         orgin = rectTransform.anchoredPosition;
+        itemInfo = GetComponentInParent<ItemSlot>();
     }
 
     public void OnBeginDrag(PointerEventData eventData){
@@ -38,4 +43,14 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
     public void OnDrag(PointerEventData eventData){
         rectTransform.position = Input.mousePosition;
     }
+
+    public void OnPointerEnter(PointerEventData eventData){
+        objectGUI.SetActive(true);
+    }
+
+     public void OnPointerExit(PointerEventData eventData){
+        objectGUI.transform.position = new Vector3(0,0,0);
+        objectGUI.SetActive(false);
+    }
+    
 }
